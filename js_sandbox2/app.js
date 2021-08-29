41dd55a32e6a5fbc6f9cd0a0e9a34152f5fe3bae
Harsh Popat <harshpopat75@gmail.com>
2021-08-29 20:08:28 +0530

    Math Object code added

---
+++ js_sandbox2/app.js
@@ -0,0 +1,29 @@
+const num1 = 100;
+const num2 = 50;
+
+//simple math
+
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+//math object
+
+val = Math.PI; //gives pi value
+
+val = Math.E; //eulers no.
+
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.pow(8, 2);
+val = Math.min(2, 3, 5, 8);
+val = Math.max(2, 3, 5, 8);
+
+val = Math.random(); //this does not give random whole no
+
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
