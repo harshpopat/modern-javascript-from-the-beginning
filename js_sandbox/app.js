b85eb445d50b8525650f127258f54275f095cbbf
Harsh Popat <harshpopat75@gmail.com>
2021-08-29 18:19:21 +0530

    playing with console

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,1 +1,24 @@
-alert('Hello World');
+// alert('Hello World');
+
+// Playing with the console
+
+console.log('Hello World');
+
+console.log(123);
+
+console.log(true);
+
+var greeting = "Hello";
+console.log(greeting);
+
+console.error('This is an error');
+
+console.warn('This is a warning')
+
+console.time('Hello');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Hello');
